ed0240660e8f297625d7b8c73367fdd1ce2397b5
Laxmikanth Dhage <laxmikanth.dhage@gmail.com>
2022-02-24 15:11:32 -0800

    Added comments

--- automation/send.py
+++ automation/send.py
@@ -6,6 +6,13 @@ connection = pika.BlockingConnection(
 )
 channel = connection.channel()
 
+# To make messages durable. Following settings are needed.
+#   1. durable = True ---> When creating queue.
+#   2. delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE ---> When publishing message
+# This will persist messages to disk.
+# Even if RabbitMQ service is restarted messages are still available.
+# When services restarted queue Hello and messages in Hello are lost.
+# while Hello1 and its messages are retrieved back from disk. 
 channel.queue_declare('hello1', durable=True)
 
 channel.basic_publish(exchange='', routing_key='hello1', body='Hello RabbitMQ!',

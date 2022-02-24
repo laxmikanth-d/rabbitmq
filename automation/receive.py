3e1d63bd4b4fab432ba13c2438f844954a6f6f90
Laxmikanth Dhage <laxmikanth.dhage@gmail.com>
2022-02-24 15:04:00 -0800

    Made queue persistant.

--- automation/receive.py
+++ automation/receive.py
@@ -3,8 +3,9 @@ import pika
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost')
 )
-
 channel = connection.channel()
+
+channel.queue_declare('hello')
 
 have_msgs = True
 
--- automation/send.py
+++ automation/send.py
@@ -4,11 +4,13 @@ from pika import channel
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost')
 )
-
 channel = connection.channel()
 
-channel.queue_declare('hello')
+channel.queue_declare('hello1', durable=True)
 
-channel.basic_publish(exchange='', routing_key='hello', body='Hello RabbitMQ!')
+channel.basic_publish(exchange='', routing_key='hello1', body='Hello RabbitMQ!',
+properties=pika.BasicProperties(
+    delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
+))
 
 connection.close()
